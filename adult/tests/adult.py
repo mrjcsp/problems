2be7196b28cbe0b86db36da66425e84bc373d8a7
Mr. Jaramillo <91510334+mrjcsp@users.noreply.github.com>
2025-09-29 18:29:40 -0500

    Create adult.py

---
+++ adult/tests/adult.py
@@ -0,0 +1,33 @@
+import check50
+import check50.py
+
+@check50.check()
+def exists():
+    """adult.py exists"""
+    check50.exists("adult.py")
+
+@check50.check(exists)
+def function_defined():
+    """Program defines a function is_adult(age)"""
+    check50.py.import_("adult").is_adult
+
+@check50.check(function_defined)
+def under_18_returns_false():
+    """is_adult(17) returns False"""
+    student = check50.py.import_("adult")
+    if student.is_adult(17) != False:
+        raise check50.Failure("is_adult(17) should return False")
+
+@check50.check(function_defined)
+def over_18_returns_true():
+    """is_adult(18) returns True"""
+    student = check50.py.import_("adult")
+    if student.is_adult(18) != True:
+        raise check50.Failure("is_adult(18) should return True")
+
+@check50.check(function_defined)
+def older_returns_true():
+    """is_adult(30) returns True"""
+    student = check50.py.import_("adult")
+    if student.is_adult(30) != True:
+        raise check50.Failure("is_adult(30) should return True")
